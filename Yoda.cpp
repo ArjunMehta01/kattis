1e0360aa2987ff37e6f8dcdc75740c8ce8ce440f
Arjun Mehta <63614270+ArjunMehta01@users.noreply.github.com>
2022-01-06 17:14:22 -0700

    Yoda Complete

--- Yoda.cpp
+++ Yoda.cpp
@@ -1,39 +1,74 @@
 #include <iostream>
+#include <string>
+#include <math.h>
 using namespace std;
 
 int main() {
+    string a;
+    string b;
+    cin >> a;
+    cin >> b;
+    int length;
+    int indexA = a.length() - 1;
+    int indexB = b.length() - 1;
+    int exponentA = 0;
+    int exponentB = 0;
 
-    int a;
-    int b;
-    int temp_a;
-    int temp_b;
+    int outA = 0;
+    int outB = 0;
 
+    if(a.length() > b.length()){
+        length = a.length();
 
-    int max_length = a >=b ? to_string(a).length() : to_string(b).length();
-
-
-    for(int i=0; i < max_length; i++){
-            temp_a = a % 10;
-            a = (a-temp_a) / 10;
-
-            temp_b = b % 10;
-            b = (b-temp_b) / 10;
-
-
-            if(temp_a > temp_b){
-                
-            }
-            else if(temp_b > temp_a){
-
-            }
-            else {
-
-            }
+    }
+    else {
+        length = b.length();
     }
 
+    for(int i = length-1; i >= 0; i--) {
+        if(indexA >= 0 && indexB >= 0){
+            if(a[indexA] > b[indexB]) {
+                outA = outA + (a[indexA] - '0') * pow(10, exponentA);
+                exponentA++;
 
+            }
+            else if(b[indexB] > a[indexA]) {
+                outB = outB + (b[indexB] -'0')* pow(10, exponentB);
+                exponentB++;
+            }
+            else {
+                outA = outA + (a[indexA] -'0')* pow(10, exponentA);
+                exponentA++;
 
+                outB = outB + (b[indexB] - '0') * pow(10, exponentB);
+                exponentB++;
+            }
+        }
+        else if(indexA < 0){
+            outB = outB + (b[indexB]-'0') * pow(10, exponentB);
+            exponentB++;
+        }
+        else {
+            outA = outA + (a[indexA] -'0')* pow(10, exponentA);
+            exponentA++;
+        }
+        indexA--;
+        indexB--;
 
-    cout << sizeof(a) << endl;
-    return 0;
+    }
+
+    if(outA == 0 && exponentA == 0){
+        cout << "YODA" << endl;
+    }
+    else {
+        cout << outA << endl;
+    }
+
+    if(outB == 0 && exponentB == 0){
+        cout << "YODA" << endl;
+    }
+    else {
+        cout << outB << endl;
+    }
+
 }
